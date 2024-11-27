071f079c1188f8c587003e6d72f0989cf9f0c088
shahart <shahart@users.noreply.github.com>
2024-11-27 15:33:41 +0200

    pwa another trial, for minimal-ui

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+if("serviceWorker" in navigator){
+    navigator.serviceWorker.register("sw.js").then(registration=>{
+      console.log("SW Registered!");
+    }).catch(error=>{
+      console.log("SW Registration Failed");
+    });
+}else{
+  console.log("Not supported");
+}
